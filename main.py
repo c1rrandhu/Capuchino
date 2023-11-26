342103dd2ad1e05dd05352785f8cc1b5bf6a625a
Arthur <avi.444@yandex.ru>
2023-11-26 21:18:14 +0300

    First commit

--- main.py
+++ main.py
@@ -433,6 +433,7 @@ class Coffee(QMainWindow):
         self.edit_btn.clicked.connect(self.openForm)
 
     def run(self):
+        self.initUI()
         for x in range(len(self.res)):
             for y in range(len(self.res[0])):
                 self.tableWidget.setItem(x, y, QTableWidgetItem(str(self.res[x][y])))
@@ -448,16 +449,31 @@ class addEditFormWidget(QMainWindow):
         f = io.StringIO(template)
         uic.loadUi(f, self)
 
-        self.status = self.comboBox.currentText()
+        self.db = sqlite3.connect('coffee.sqlite')
         self.apply_btn.clicked.connect(self.apply_info)
 
     def apply_info(self):
-        data = [self.id.text(), self.name.text(), self.roasting.text(), self.type.text(),
-                self.description.text(), self.price.text(), self.weigh.text()]
-        if self.status == 'Добавить':
-            pass
-        else:
-            pass
+        try:
+            status = self.comboBox.currentText()
+            self.statusBar().showMessage('')
+            cur = self.db.cursor()
+            db_id = cur.execute('SELECT id FROM main').fetchall()[-1][0]
+
+            data = [self.id.text(), self.name.text(), self.roasting.text(), self.type.text(),
+                    self.description.text(), self.price.text(), self.weigh.text()]
+
+            if status == 'Добавить':
+                cur.execute(f'''INSERT INTO main VALUES({int(db_id) + 1}, "{data[1]}", 
+                                    "{data[2]}", "{data[3]}", "{data[4]}", "{data[5]}", "{data[6]}")''')
+            else:
+                if int(db_id) < int(data[0]) or int(data[0]) <= 0:
+                    raise Exception
+                cur.execute(f'''UPDATE main SET name = "{data[1]}", roasting = "{data[2]}", type = "{data[3]}", 
+                        description = "{data[4]}", price = "{data[5]}", weigh = "{data[6]}" WHERE id = "{data[0]}"''')
+            self.statusBar().showMessage('Запрос выполнен!')
+            self.db.commit()
+        except Exception:
+            self.statusBar().showMessage('ERROR!')
 
 
 if __name__ == '__main__':
